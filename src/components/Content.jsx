8d02b7a9c0f19994a643cdebd4c05c41a5b98ba4
q2lamko <Avenger_adamant@mail.ru>
2020-11-27 14:52:30 +0700

    zalupa

---
+++ src/components/Content.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src="https://i.playground.ru/p/Va7pSKtxxjf3QRTzLHMbRg.jpeg" width="400" height="400"></img>
+      </div>
+      <div>
+        <img src="https://i.pinimg.com/236x/20/fa/9d/20fa9d7a51baab636700e9680fa4b7f7.jpg" />
+      </div>
+      <div>
+        post
+          <div>
+          new post
+          </div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+
+
+      </div>
+
+    </div>)
+}
+
+export default Content;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <header className='header'>
+        <img src='https://i.pinimg.com/736x/48/3a/6c/483a6c355deffde74c84cb5ca2636ee4--logo-software-security-logo.jpg'></img>
+    </header>)
+}
+
+export default Header;
